56b0f5c81c6aac8b47c8d484db8fb44693560a42
mehdi <mahdydarvish@gmail.com>
2021-12-24 12:22:28 +0330

    migrations are done and superuser created (username: MATAM, password: 111)

--- car_insurance/main/admin.py
+++ car_insurance/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Car, Insurance_Agent, Policy, Customer, Notification
+
+admin.site.register(Car)
+admin.site.register(Insurance_Agent)
+admin.site.register(Policy)
+admin.site.register(Customer)
+admin.site.register(Notification)
--- car_insurance/main/models.py
+++ car_insurance/main/models.py
@@ -1,23 +1,31 @@
 from django.db import models
 
+
 class Customer(models.Model):
+
     customer_id = models.AutoField(primary_key=True)
     customer_name = models.CharField(max_length=50)
     adress = models.TextField()
     email = models.EmailField()
     phone = models.IntegerField()
 
+
 class Car(models.Model):
+
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    engine_number = models.Int(primary_key=True)
+    engine_number = models.IntegerField(primary_key=True)
     model = models.CharField(max_length=50)
     manufacture_year = models.DateField()
 
+
 class Insurance_Agent(models.Model):
+
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     agent_id = models.AutoField(primary_key=True)
 
+
 class Policy(models.Model):
+
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
     agent_id = models.ForeignKey(Insurance_Agent, on_delete=models.CASCADE)
     engine_number = models.ForeignKey(Car, on_delete=models.CASCADE)
@@ -28,6 +36,8 @@ class Policy(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
 
+
 class Notification(models.Model):
+
     policy_id = models.ForeignKey(Customer, on_delete=models.CASCADE, primary_key=True)
     date = models.DateTimeField(null=True)
