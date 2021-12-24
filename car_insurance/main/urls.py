3c15802a13eca48779b8867431e701a149d06a6f
Ashkan <AshkanGanj@gmail.com>
2021-12-24 20:15:31 +0330

    finish car and policy UI

--- car_insurance/main/urls.py
+++ car_insurance/main/urls.py
@@ -3,10 +3,27 @@ from . import views
 
 
 urlpatterns = [
+    # customer
     path('', views.listCustomers, name='index'),
     path('customer/add', views.addCustomer, name='addCustomer'),
     path('customer/edit', views.editCustomer, name='editCustomer'),
-    path('customer/editProfile', views.editCustomerProfile, name='editCustomerProfile'),
+    path('customer/editProfile', views.editCustomerProfile,
+         name='editCustomerProfile'),
     path('customer/get', views.getCustomer, name='getCustomer'),
     path('customer/list', views.listCustomers, name='listCustomers'),
+    # car
+    path('car', views.listCars, name='index'),
+    path('car/add', views.addCar, name='addCar'),
+    path('car/edit', views.editCar, name='editCar'),
+    path('car/get', views.getCar, name='getCar'),
+    path('car/list', views.listCars, name='listCars'),
+    path('car/delete', views.deleteCar, name='deleteCar'),
+    # Insurance
+    path('insurance', views.listPolicies, name='index'),
+    path('insurance/add', views.addPolicy, name='addPolicy'),
+    path('insurance/edit', views.editPolicy, name='editPolicy'),
+    path('insurance/delete', views.deletePolicy,
+         name='deletePolicy'),
+    path('insurance/get', views.getPolicy, name='getPolicy'),
+    path('insurance/list', views.listPolicies, name='listPolicies'),
 ]
--- car_insurance/main/views.py
+++ car_insurance/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Customer
+
+
 def addCustomer(request):
     return render(request, 'Customer/create.html', {})
 
@@ -20,7 +23,48 @@ def getCustomer(request):
 def listCustomers(request):
     return render(request, 'Customer/list.html', {})
 
+# Car
 
 
-def deleteCare(request):
+def addCar(request):
+    return render(request, 'Car/create.html', {})
+
+
+def editCar(request):
+    return render(request, 'Car/edit.html', {})
+
+
+def getCar(request):
+    return render(request, 'Car/get.html', {})
+
+
+def listCars(request):
+    return render(request, 'Car/list.html', {})
+
+
+def deleteCar(request):
     return render(request, 'Car/delete.html', {})
+# ---------------------------
+
+# policy
+
+
+def addPolicy(request):
+    return render(request, 'insurance/create.html', {})
+
+
+def editPolicy(request):
+    return render(request, 'insurance/edit.html', {})
+
+
+def getPolicy(request):
+    return render(request, 'insurance/get.html', {})
+
+
+def listPolicies(request):
+    return render(request, 'insurance/list.html', {})
+
+
+def deletePolicy(request):
+    return render(request, 'insurance/delete.html', {})
+# ---------------------------
