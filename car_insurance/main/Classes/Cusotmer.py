afaf600c132fa8d9691e82e75d21494efbb0adab
Ashkan <AshkanGanj@gmail.com>
2021-12-24 16:56:54 +0330

    add customer class attr and methods

---
+++ car_insurance/main/Classes/Cusotmer.py
@@ -0,0 +1,22 @@
+class Customer:
+    def __init__(self, address, name, customerID, email, phone):
+        self.address = address
+        self.name = name
+        self.customerID = customerID
+        self.email = email
+        self.phone = phone
+
+    def create(self):
+        pass
+
+    def get(self, customerID):
+        pass
+
+    def update(self, customerID):
+        pass
+
+    def updateProfile(self):
+        pass
+
+    def list(self):
+        pass
