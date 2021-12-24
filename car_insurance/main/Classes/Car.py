7393e45c751b3785da255b1d2ee44bdf35024dfc
Ashkan <AshkanGanj@gmail.com>
2021-12-24 17:00:03 +0330

    add car class's methods and attr

---
+++ car_insurance/main/Classes/Car.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self, engineNum, Model, YearOfManufacture):
+        self.engineNum = engineNum
+        self.Model = Model
+        self.YearOfManufacture = YearOfManufacture
+
+    def add(self):
+        pass
+
+    def get(self, engineNum):
+        pass
+
+    def edit(self, engineNum):
+        pass
+
+    def delete(self, engineNum):
+        # policy should deleted too
+        pass
+
+    def list(self):
+        pass
