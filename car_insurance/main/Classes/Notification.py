b3e777e2f2616f5fc549da5bfcea4b46cb4b9d97
Ashkan <AshkanGanj@gmail.com>
2021-12-25 18:02:26 +0330

    coment notif

--- car_insurance/main/Classes/Notification.py
+++ car_insurance/main/Classes/Notification.py
@@ -1,32 +1,32 @@
 from django.core.mail import send_mail
 from main.models import Notification_table, Policy_table
 
-class Notification:
-    def __init__(self):
-        self.title = 'insurance'
-        self.policies = []
+# class Notification:
+#     def __init__(self):
+#         self.title = 'insurance'
+#         self.policies = []
         
 
-    def check(self):
-        list_policy =  Policy_table.objects.all()
-        for item in list_policy:
-            print(item.end_date.timestamp())
-            left = item.end_date - item.start_date
-            # if left < 6:
-            #     self.policies.append(item)
-        print(list_policy)
-        self.check()
+#     def check(self):
+#         list_policy =  Policy_table.objects.all()
+#         for item in list_policy:
+#             print(item.end_date.timestamp())
+#             left = item.end_date - item.start_date
+#             # if left < 6:
+#             #     self.policies.append(item)
+#         print(list_policy)
+#         self.check()
 
  
-    def send(self):
-        receivers = []
-        for item in self.policies:
-            receivers.append(item.email)
-        send_mail(
-        self.title,
-        'Your Insurance is about to expire, Please recharge it a soon as possible.',
-        'ganj.ashkan@gmail.com',
-        receivers,
-        fail_silently=False)
-        print('sent')
+#     def send(self):
+#         receivers = []
+#         for item in self.policies:
+#             receivers.append(item.email)
+#         send_mail(
+#         self.title,
+#         'Your Insurance is about to expire, Please recharge it a soon as possible.',
+#         'ganj.ashkan@gmail.com',
+#         receivers,
+#         fail_silently=False)
+        # print('sent')
 
