35a54f749ec7d7b3724f992a70514db127036c27
mehdi <mahdydarvish@gmail.com>
2021-12-25 19:39:38 +0330

    added email

--- car_insurance/main/views.py
+++ car_insurance/main/views.py
@@ -12,19 +12,9 @@ import time
 def job():
     notif = Notification()
     notif.send()
-    print("send")
+    print("Checked for policy Expiration date")
 
-def run_continuously(self, interval=1):
-    """Continuously run, while executing pending jobs at each elapsed
-    time interval.
-    @return cease_continuous_run: threading.Event which can be set to
-    cease continuous run.
-    Please note that it is *intended behavior that run_continuously()
-    does not run missed jobs*. For example, if you've registered a job
-    that should run every minute and you set a continuous run interval
-    of one hour then your job won't be run 60 times at each interval but
-    only once.
-    """
+def run_continuously(self, interval=1000):
 
     cease_continuous_run = threading.Event()
 
